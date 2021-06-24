148dae859b616036d2f6a590377048451d2a4230
alska95 <rhsalska55@naver.com>
2021-06-24 23:45:49 +0900

    상품 수정 , merge에서 변경감지로 수정

--- src/main/java/com/example/demo/service/ItemService.java
+++ src/main/java/com/example/demo/service/ItemService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 
+import com.example.demo.domain.item.Book;
 import com.example.demo.domain.item.Item;
 import com.example.demo.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +20,19 @@ public class ItemService {
         this.itemRepository = itemRepository;
     }
 
+    @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
     }
 
+    @Transactional
+    public void updateItem(Long itemId, int price  , int stock , String name){
+        Item findItem = itemRepository.findById(itemId);
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stock);
+
+    }
     public List<Item> findItems(){
         return itemRepository.findAll();
     }
