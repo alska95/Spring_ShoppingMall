c7ad7ffe8fdec3107d503a939c85dbd87726fd33
alska95 <rhsalska55@naver.com>
2021-09-24 06:58:14 +0900

    회원 조회 API 추가

--- src/main/java/com/example/demo/api/MemberApiController.java
+++ src/main/java/com/example/demo/api/MemberApiController.java
@@ -8,12 +8,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    } //이렇게 하면 원하는것만 노출할 수 없다 --> DTO(Response Object)의 필요.
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2(){
+        List<Member> members = memberService.findMembers();
+        List<MemberDto> collect = members.stream().map(v -> new MemberDto(v.getName())).collect(Collectors.toList());
+        return new Result(collect.size() ,collect);
+    } //이름만 Result로 감싸서 노출 --> 그냥 노출시키면 단순 json배열로 변환되기 때문에 유연성이 떨어진다.
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
@@ -41,7 +54,6 @@ public class MemberApiController {
     @Data
     static class CreateMemberResponse{
         private Long id;
-
         public CreateMemberResponse(Long id) {
             this.id = id;
         }
@@ -49,9 +61,7 @@ public class MemberApiController {
 
     @Data
     private class CreateMemberRequest {
-
         private String name;
-
         public CreateMemberRequest(String name) {
             this.name = name;
         }
@@ -67,5 +77,18 @@ public class MemberApiController {
     @Data
     private class UpdateMemberRequest {
         private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    private class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    private class MemberDto {
+        String name;
     }
 }
